eabf9c4594b85536d05bc0a63909e7209f1d9afe
Venuri Nuwanga <nuwangawatagedar@gmail.com>
2025-10-07 17:47:05 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,4 @@
-const { admin } = require('../../config/firebase');
-const db = admin.firestore();
+const { admin, db } = require('../config/firebase');
 const usersCollection = db.collection('users');
 
 class User {
